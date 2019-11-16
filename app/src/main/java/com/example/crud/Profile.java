0418a0cc827c465599b97525e4f6bc7097e403f6
duple <duplessis.p.andre@gmail.com>
2019-11-16 16:03:19 +0200

    Initial Commit

--- app/src/main/java/com/example/crud/Profile.java
+++ app/src/main/java/com/example/crud/Profile.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
-
+int pc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
